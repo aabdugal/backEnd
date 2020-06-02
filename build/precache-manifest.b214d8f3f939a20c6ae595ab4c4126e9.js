0986d01ba66c9a97e84cd9477bf96af2d767c744
Adil <aabdugal@gmail.com>
2020-06-02 15:59:24 +0600

    added index

---
+++ build/precache-manifest.b214d8f3f939a20c6ae595ab4c4126e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f0cce68e445366983c4ec5ce7ace89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f325bff32a7fa185f7ea",
+    "url": "/static/css/main.71297600.chunk.css"
+  },
+  {
+    "revision": "2c3addb016d5b033f862",
+    "url": "/static/js/2.bb78e327.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.bb78e327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f325bff32a7fa185f7ea",
+    "url": "/static/js/main.e3c34304.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
